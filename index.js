20d06a01f1e36f85bab8b00220477194d94070ee
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-06-11 23:27:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const taxiDriver = drivers.filter(
+        (match) => match.toLowerCase() === name.toLowerCase());
+    return taxiDriver;
+}
+function fuzzyMatch(drivers, name) {
+    const taxiDriver = drivers.filter(
+        (match) => match.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+    return taxiDriver;
+}
+function matchName(driver, name) {
+    const taxiDriver = driver.filter(
+        (match) => match.name === name
+    )
+    return taxiDriver;
+
+}
